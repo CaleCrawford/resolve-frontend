57e4d85bf72b79b03d358e9555a8f99c67b5f460
Cale Crawford <cale@typeninecreatives.com>
2021-01-18 18:58:03 -0800

    Test 3; ;

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
-module.exports = {
-  /* config options here */
-  images: {
-    domains: ['localhost', 'resolve-blog.herokuapp.com'],
-  },
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+// const withImages = require('next-images')
+
+module.exports = withPlugins(
+  [
+    [optimizedImages, {
+      /* config for next-optimized-images */
+      handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+    }],
+
+    // your other plugins here
+    // [withImages, {
+    //   // assetPrefix: 'resolve-blog.herokuapp.com',
+    //   // dynamicAssetPrefix: true,
+    //   // webpack(config, options) {
+    //   //   return config
+    //   // }
+    //   inlineImageLimit: false
+    // }]
+  ],
+  {
+    /* config options here */
+    images: {
+      loader: 'cloudinary',
+      domains: ['localhost', 'resolve-blog.herokuapp.com'],
+    },
+  }
+);
